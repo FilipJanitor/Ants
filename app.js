07be142e0b4b938e5189490f4a7ec81b3ee65a47
FilipJanitor <filip.janitor@gmail.com>
2018-04-05 23:04:15 +0200

    Add basic connections

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-var mysql = require('mysql')
+const express = require('express');
+const mysql = require('mysql');
+const bcrypt = require('bcryptjs');
+const schemaValidator = require('ajv');
+const bodyParser = require('body-parser')
+const app = express();
+
+if(process.argv.length != 3){
+    console.log("Invalid arguments provided. Aborting!");
+    process.exit(-1);
+}
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.argv[2],
+    database: 'main',
+    port: 3306
+});
+
+const server = app.listen(8080, function(){
+    console.log("Server listening...");
+});
+
+db.connect((err) => {
+    if(err){
+        console.log("Connecting with database failed. Aborting!");
+        console.log(err);
+        process.exit(-1);
+    } else {
+        console.log("Connection to database estabilished.");
+    }
+});
+
+app.use(bodyParser.json());
