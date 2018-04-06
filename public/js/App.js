bea828b7f41e992746b042affc2e4efedd9d41c8
FilipJanitor <filip.janitor@gmail.com>
2018-04-06 22:52:20 +0200

    Add parts to route

--- public/js/App.js
+++ public/js/App.js
@@ -3,3 +3,48 @@ import { connect } from "react-redux";
 
 
 //hlavny komponent
+class MainComponent extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div class="page-header">
+                    <Header />
+                </div>
+                <div>
+                    {this.props.children} //sem davame komponentove deti z routru    
+                </div>
+                <div class="page-footer">
+                    <Footer />
+                </div>
+            </div>   
+        )
+    }
+}
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(props){
+        return(
+            <h1> Ants </h1>
+        )
+    }
+}
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    //TODO copyright info
+    render(props){
+        return(
+            <footer> TODO legal info </footer>
+        )
+    }
+}
