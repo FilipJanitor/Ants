23f5b28d55168d2788e57ec908b87f7e86dc948b
FilipJanitor <filip.janitor@gmail.com>
2018-04-06 11:49:32 +0200

    Add schema validation and begin login

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const validator = new Validator({allErrors: true});
 const validate = validator.validate;
 const app = express();
 
+const catchValidationErrors = function(err,req,res,next) {
+    console.log(err.ValidationErrors);
+    res.send({result: false, error: 'SchemaError'});
+}
+
 if(process.argv.length != 3){
     console.log("Invalid arguments provided. Aborting!");
     process.exit(-1);
@@ -35,4 +40,36 @@ db.connect((err) => {
 });
 
 //sample usage
-//app.post('/route',validate({requestProperty: schemaToUse}), bodyParser.json(), function(req,res){res.send()});
+//app.post('/route',validate({requestProperty: schemaToUse}), bodyParser.json(), catchValidationErrors, function(req,res){res.send()});
+
+// __________________basics____________________________________________________
+
+app.get('/client.bundle.js', function(req,res){
+    console.log('send js TODO');
+    res.sendFile(__dirname + '/public/client.bundle.js');
+});
+
+app.get('/style.css', function(req,res){
+    console.log('send css TODO');
+    res.sendFile(__dirname + '/public/style.css');
+});
+
+// __________________interaction_______________________________________________
+
+app.post('/login',validate({body: loginSchema}), bodyParser.json(), catchValidationErrors, function(req,res){
+    const data = req.body;
+    const query = 'SELECT token, userId FROM users where name=' + db.escape(data.name) + ' AND password=sha2(' + db.escape(data.password) + ')';
+    db.query(query, (err, rows, fields) => {
+        if(err){
+            consloe.log(err);
+            res.send({result: false, error: 'LoginError'});
+            return;
+        }
+        if(rows.length == 1){
+
+        } else {
+            res.send({result: false, error: 'LoginError'});
+        }
+    });
+});
+
