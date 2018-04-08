d609d0977ee21404ab4edd90c7e4c741724e2389
FilipJanitor <filip.janitor@gmail.com>
2018-04-08 21:25:47 +0200

    Up and running. Cleaned imports

--- public/js/client.js
+++ public/js/client.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./Login.js";
-import Lobby from "./Lobby.js";
-import Game from "./Game.js";
-import Register from "./Register.js";
-import reducer from "./reducers/combinedReducers.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Login.js';
+import Lobby from './Lobby.js';
+import Game from './Game.js';
+import Register from './Register.js';
+import NotFound from './NotFound.js';
+import reducer from './reducers/combinedReducers.js';
 
 const store = createStore(reducer);
 
