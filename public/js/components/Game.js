aa08940900dc990e4ff42c60f64598e5240c0e95
FilipJanitor <filip.janitor@gmail.com>
2018-04-23 23:24:26 +0200

    Started styling gmae window

--- public/js/components/Game.js
+++ public/js/components/Game.js
@@ -11,8 +11,38 @@
 +---+-+-+-+-+-+-+-+-+---+
 */
 
-export default (props) => {
-    return (
-        TODO
-    );
+//da sa to zlozit z viacero komponentov. Tak
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() { //gamewrapper bude maintainovat aspect ratio. V Cards musi byt mapstatetoprops
+        return (
+            <div id="gameWrapper">
+                <div className="gameBackground">
+                    <img className="autoFill" src="/background" />
+                </div>
+                <div className="statsWrapper">
+                    <Stats type="left" stats={this.props.appState.playerStats} />
+                    <Tower type="left" stats={this.props.appState.playerStats} />
+                    <Tower type="right" stats={this.props.appState.opponentStats} />
+                    <Stats type="right" stats={this.props.appStatle.opponentState} />
+                </div>
+                <div className="cardsWrapper">
+                    <Cards/>
+                </div>
+            </div>
+        );
+    }
 }
+
+export default withRouter(connect(state => {
+    return {
+        appState: state.appState
+    };
+})(Game));
